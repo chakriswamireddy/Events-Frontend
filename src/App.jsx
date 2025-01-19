689c0fc5f18cc0cd98a388f27b0e0c65ca2865c1
chakriswamireddy <iamthechakri@gmail.com>
2025-01-20 00:05:02 +0530

    redirecting from /

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import {BrowserRouter,Link,Navigate,Route, Routes} from 'react-router-dom'
+import {BrowserRouter,Navigate,Route, Routes} from 'react-router-dom'
 import AuthorizeUser from './mycomponents/AuthorizeUser'
 import Homepage from './mycomponents/Homepage'
 import 'animate.css';
-import { Home } from 'lucide-react'
 
 function App() {
-
-  
-
-  useEffect(()=> {
-
-  },[])
 
   
 
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={<div> HI <Home /> <Link to='/authorize'> Enter  </Link>  </div>} />
+      <Route path='/' element={<Navigate to='/authorize' />} />
 
       <Route path='/authorize' element={<AuthorizeUser /> } />
       
